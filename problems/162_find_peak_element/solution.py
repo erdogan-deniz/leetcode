5024e974527aed76966f19bd4570e5fe3773f2df
Deniz <erdogan33@mail.ru>
2025-08-09 17:41:13 +0300

    Solve 162.

---
+++ problems/162_find_peak_element/solution.py
@@ -0,0 +1,24 @@
+"""
+Solution: binary search.
+
+Time complexity: O(n).
+Space complexity: O(1).
+"""
+
+class Solution:
+    """
+    ...
+    """
+
+    def find_peak_element(self, nums: list[int]) -> int:
+        """
+        ...
+        """
+
+        left_idx: int = 0
+        right_idx: int = 0
+
+        while left_idx < right_idx:
+            
+
+        return nums[left_idx]
